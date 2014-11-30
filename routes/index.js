6c5d7bdc686aea985403d49ae8eee87268be372a
Kaelig <kaelig@deloumeau.fr>
2014-11-30 23:17:12 +0000

    Fix asset type on the front-end

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ var _ = require('lodash');
 var express = require('express');
 var md5 = require('MD5');
 var router = express.Router();
-var path = require('path');
+var utils = require('./utils');
 
 router.get('/', function(req, res) {
   var assets = _.map(_.keys(res.locals.config.assets), function(asset) {
     return {
       name: asset,
       hash: md5(asset),
-      type: path.extname(res.locals.config.assets[asset]).slice(1)
+      type: utils.getAssetType(res.locals.config.assets[asset])
     };
   });
   res.render('index', { title: 'moniteur', assets: assets });
