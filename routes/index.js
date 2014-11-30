9148790349f5cfd8c8b0847b3a2b85197851a03e
Kaelig <kaelig@deloumeau.fr>
2014-11-30 23:23:43 +0000

    Fix path to utils

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 var express = require('express');
 var md5 = require('MD5');
 var router = express.Router();
-var utils = require('./utils');
+var utils = require('../lib/utils');
 
 router.get('/', function(req, res) {
   var assets = _.map(_.keys(res.locals.config.assets), function(asset) {
