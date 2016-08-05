e553cf7be89a7c15119f084b8fbf5b868793e9a2
Kaelig <kaelig@deloumeau.fr>
2016-08-04 21:53:04 -0700

    Rewrite the metrics fetching method in functional JS

--- routes/metrics.js
+++ routes/metrics.js
@@ -1,23 +1,46 @@
 import express from 'express'
-import Read from '../lib/read'
 import debug from 'debug'
 import lem from 'lem'
+import sensors from '../lib/sensors'
+import highland from 'highland'
 
+const _ = highland
 const router = express.Router()
 const log = debug('moniteur:log')
 
 // Example:
 // Series (since forever): /metrics/css/adf6e9c154cb57a818f7fb407085bff6
 // Series between two dates: /metrics/css/adf6e9c154cb57a818f7fb407085bff6/1015711104475..1415711104475
-export default router.get(/^\/(\w+)\/(\w+)(\/(\d+)\.\.(\d+))?$/, (req, res, next) => {
+export default router.get(/^\/(\w+)\/(\w+)(\/(\d+)\.\.(\d+))?$/, (req, res) => {
   const assetType = req.params[0]
   const assetHash = req.params[1]
-  const start = req.params[3] || false
-  const end = req.params[4] || false
+  const start = req.params[3] || (Date.now() - 3600 * 24 * 30 * 1000)
+  const end = req.params[4] || Date.now()
+  const db = lem(res.locals.db)
 
-  const read = new Read(assetType, assetHash, start, end, res.locals.assets, lem(res.locals.db))
-
-  Promise.all(read.getMetrics()).then((data) => {
-    res.json(data, null, 4)
-  }).catch(next)
+  // Get Array of values for metric
+  _(Object.keys(sensors[assetType])).flatMap((metric) => {
+    const key = `assets.${assetHash}.${metric}`
+    log('Opening ValueStream for:', key)
+    return _(db.valuestream(
+      key,
+      {
+        start: start,
+        end: end
+      }
+    ))
+      .filter((data) => data.value !== 0)
+      .map((data) =>
+        [
+          data.key,
+          data.value
+        ]
+      )
+      .collect()
+      .map((data) =>
+        Object.assign(
+          sensors[assetType][metric],
+          { data: data }))
+  }).toArray((data) =>
+    res.json(data, null, 4))
 })
