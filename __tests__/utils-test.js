6987d6c4009a09f270a99f2b85560ec21944dadc
Kaelig <kaelig@deloumeau.fr>
2016-08-30 20:55:55 -0700

    Convert getAssetType to functional JS

--- __tests__/utils-test.js
+++ __tests__/utils-test.js
@@ -22,7 +22,7 @@ describe('assetType', () => {
   it('returns the correct type', () => {
     expect(utils.getAssetType('/path/something.css')).toEqual('css')
     expect(utils.getAssetType('/path/to.html')).toEqual('html')
-    expect(utils.getAssetType('http://css.com/something.css')).toEqual('css')
+    expect(utils.getAssetType('http://js.com/something.css')).toEqual('css')
     expect(utils.getAssetType(['http://bar.com/something.css'])).toEqual('css')
   })
 })
