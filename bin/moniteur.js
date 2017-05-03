3e68084b9c6a2cee165b3bb1ce4992acb172fe51
Kaelig Deloumeau-Prigent <kaelig@users.noreply.github.com>
2017-05-02 20:09:11 -0700

    Add support for basic auth
    Set USERNAME and PASSWORD global variables to turn on Basic Auth

--- bin/moniteur.js
+++ bin/moniteur.js
@@ -5,6 +5,7 @@ const Record = require('../lib/record')
 const nconf = require('nconf')
 const compression = require('compression')
 const express = require('express')
+const auth = require('http-auth')
 const slashes = require('express-slashes')
 const path = require('path')
 const lem = require('lem')
@@ -65,6 +66,19 @@ program
   .description('start the server to show metrics in the browser')
   .action(() => {
     const app = express()
+    
+    // Basic auth
+    // Set USERNAME and PASSWORD environment variables
+    const basic = auth.basic({
+      realm: 'Moniteur'
+    }, (username, password, next) => {
+      next(username === process.env.USERNAME && password === process.env.PASSWORD)
+    })
+
+    if (process.env.USERNAME && process.env.PASSWORD) {
+      app.use(auth.connect(basic))
+    }
+    
     app.set('strict routing', true)
     const router = express.Router({
       caseSensitive: app.get('case sensitive routing'),
